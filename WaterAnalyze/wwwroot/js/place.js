62827e552e7a0625e14ea5e425171792c0f8cfb2
Zilya <galievaaziz@gmail.com>
2024-05-09 15:11:02 +0300

    new changes

--- WaterAnalyze/wwwroot/js/place.js
+++ WaterAnalyze/wwwroot/js/place.js
@@ -12,25 +12,19 @@ function deleteMap() {
 
 function map(x, y) {
     myMap = new ymaps.Map('map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
-        center: [x, y], // Москва
+        center: [x, y], 
         zoom: 10
     }, {
         searchControlProvider: 'yandex#search'
     });
 
     myGeoObject = new ymaps.GeoObject({
-        // Описание геометрии.
         geometry: {
             type: "Point",
             coordinates: [x, y]
         },
-        // Свойства.
         properties: {
             // Контент метки.
-            //iconContent: '',
-            //hintContent: 'Ну давай уже тащи'
         }
     });
 

e97ac7e77879417808a8b7d345164817e975a998
Zilya <galievaaziz@gmail.com>
2024-05-12 23:17:30 +0300

    +pdf

---
+++ WaterAnalyze/Data/ExportService.cs
@@ -0,0 +1,46 @@
+﻿using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Grid;
+using Syncfusion.Drawing;
+
+namespace WaterAnalyze.Data
+{
+    public class ExportService
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public ExportService(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        //Export weather data to PDF document.
+        public MemoryStream CreatePdf(Source source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("Ошибка");
+            }
+            PdfDocument document = new PdfDocument();
+            PdfPage currentPage = document.Pages.Add();
+            SizeF clientSize = currentPage.GetClientSize();
+            FileStream imageStream = new FileStream(_hostingEnvironment.WebRootPath + "//img//0.png", FileMode.Open, FileAccess.Read);
+            PdfImage icon = new PdfBitmap(imageStream);
+            SizeF iconSize = new SizeF(40, 40);
+            PointF iconLocation = new PointF(14, 13);
+            PdfGraphics graphics = currentPage.Graphics;
+            graphics.DrawImage(icon, iconLocation, iconSize);
+            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 20, PdfFontStyle.Bold);
+            var headerText = new PdfTextElement(source.Title, font, new PdfSolidBrush(Color.FromArgb(1, 53, 67, 16)));
+            headerText.StringFormat = new PdfStringFormat(PdfTextAlignment.Right);
+            PdfLayoutResult result = headerText.Draw(currentPage, new PointF(clientSize.Width - 25, iconLocation.Y + 10));
+
+            MemoryStream stream = new MemoryStream();
+            document.Save(stream);
+            document.Close(true);
+            stream.Position = 0;
+            //Create a new PDF document.
+
+            return stream;
+        }
+    }
+}
---
+++ WaterAnalyze/Data/PdfGenerator.cs
@@ -0,0 +1,48 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+
+namespace WaterAnalyze.Data
+{
+	public class PdfGenerator
+	{
+		public static void GeneratePdf(Source source, Analyze analyze, Direction direction)
+		{
+			//Create a new document
+			Document document = new Document();
+
+			//Create a PDF writer
+			PdfWriter.GetInstance(document, new FileStream("C:\\Users\\Zilya\\source\\repos\\WaterAnalyze\\WaterAnalyze\\wwwroot\\files\\example.pdf", FileMode.Create));
+
+			//Open the document
+			document.Open();
+
+			string ttf = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "ARIAL.TTF");
+			var baseFont = BaseFont.CreateFont(ttf, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+			var font = new iTextSharp.text.Font(baseFont, iTextSharp.text.Font.DEFAULTSIZE, iTextSharp.text.Font.NORMAL);
+
+			//Add a title
+			Paragraph titleParagraph = new Paragraph(source.Title, font);
+			titleParagraph.Alignment = Element.ALIGN_CENTER;
+			document.Add(titleParagraph);
+
+			//Add some text
+
+			Paragraph bodyParagraph = new Paragraph($"Наименование: {source.Title}" + "\n" + $"Координаты: {source.CoordinatesX} {source.CoordinatesY} " +
+				"\n" + "\n" + $"Направление: {direction.Title}" +
+				"\n" + $"Информация по последнему анализу:" +
+				"\n" + $"PH: {analyze.AcidityIndex}" +
+				"\n" + $"Cl: {analyze.Сhloride}" +
+				"\n" + $"HCO3: {analyze.Gidrocorbonat}" +
+				"\n" + $"SO4: {analyze.Sulfate}" +
+				"\n" + $"Ca: {analyze.Calcium}" +
+				"\n" + $"Mg: {analyze.Magnesium}" +
+				"\n" + $"Нефтепродукты: {analyze.Oil}", font);
+			document.Add(bodyParagraph);
+
+			//Close the document
+			document.Close();
+		}
+	}
+}
